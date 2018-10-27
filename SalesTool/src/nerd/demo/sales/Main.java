0852393b5b819974caa87c9a48b26f4557889c6f
Akinfenwa Phemmy <phemmyster@yahoo.com>
2018-10-27 10:03:28 +0100

    initial checking of sales tools

---
+++ SalesTool/src/nerd/demo/sales/Main.java
@@ -0,0 +1,20 @@
+package nerd.demo.sales;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		SalesData data = new SalesData();
+		
+		displayGreeting();
+		data.display();
+
+	}
+	
+	public static void displayGreeting(){
+		System.out.println("HELLO HAPPY SALE PEOPLE!");
+		System.out.println("THIS APP SALES DATA");
+	}
+
+}
---
+++ SalesTool/src/nerd/demo/sales/SalesData.java
@@ -0,0 +1,11 @@
+package nerd.demo.sales;
+
+public class SalesData {
+	
+	int Data[] = {0,4,42};
+	
+	public void display(){
+		System.out.println("TODO: Implete data display");
+	}
+
+}
